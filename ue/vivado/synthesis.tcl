9eb0cab56a55bebb25038db635fc31c1da0f78d7
fifoteam <haitaox2008@163.com>
2019-02-11 15:35:19 +0800

    AUTO SAVED BY BAT FILE

--- ue/vivado/synthesis.tcl
+++ ue/vivado/synthesis.tcl
@@ -1,15 +1,5 @@
-#Assemble the Design Source files
-read_verilog      F:/grey_statis/src/grey_aoi_sel.v
-#read_vhdl [glob ../src/*.vhd]
-#read_edif ../netlist/black_blox.edf
-read_xdc src/template.xdc
-
-
-
-#Run Synthesis and Implementation
-synth_design -top grey_aoi_sel -part xc7a100tfgg484-2
-
-
-#Generate Reports
-report_utilization -hierarchical -verbose  -file utilization_summary.rpt
-report_timing_summary -file timing_summary.rpt
+read_verilog  F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/frame_info/data_align.v
+read_xdc src/template.xdc 
+synth_design -top data_align -part xc7a100tfgg484-2 
+report_utilization -file utilization_summary.rpt 
+report_timing_summary -file timing_summary.rpt 
