92531b1e67beb5da068f23d4f8815bc0afc85ead
fifoteam <haitaox2008@163.com>
2019-02-13 19:21:35 +0800

    AUTO SAVED BY BAT FILE

--- ue/vivado/synthesis.tcl
+++ ue/vivado/synthesis.tcl
@@ -1,5 +1,5 @@
-read_verilog  F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/gige_vision/gvsp_tx/ip_udp_gvsp_format.v
+read_verilog  F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/gige_vision/gvsp_tx/packet_image.v
 read_xdc src/template.xdc 
-synth_design -top ip_udp_gvsp_format -part xc7a100tfgg484-2 
+synth_design -top packet_image -part xc7a100tfgg484-2 
 report_utilization -file utilization_summary.rpt 
 report_timing_summary -file timing_summary.rpt 
