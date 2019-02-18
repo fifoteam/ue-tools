c0595d78b9c465e28d294bff520eb90b0d2aea26
fifoteam <haitaox2008@163.com>
2019-02-18 20:13:46 +0800

    AUTO SAVED BY BAT FILE

--- ue/synplify/synthesis.tcl
+++ ue/synplify/synthesis.tcl
@@ -1,9 +1,10 @@
 project -new D:/tools/Synopsys/fpga_J-2015.03/bin/mbin/proj_1.prj
-add_file -verilog f:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/gige_vision/gvsp_tx/gvsp_ctrl_core.v
+add_file -verilog F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/frame_buffer/fb_arbiter.v
 set_option -part XC7A100T
 set_option -grade -2
 set_option -package FGG484
 set_option -enable_nfilter 1
-set_option -top_module gvsp_ctrl_core
+project -log_file c:/ue/synplify/rev_2/proj_1.srr
+set_option -top_module fb_arbiter
 project -save proj_1 c:/ue/synplify/proj_1.prj 
 project -run  -bg 
--- ue/vivado/synthesis.tcl
+++ ue/vivado/synthesis.tcl
@@ -1,5 +1,5 @@
-read_verilog  f:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/gige_vision/gvsp_tx/gvsp_ctrl_core.v
+read_verilog  F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/frame_buffer/fb_write/fb_write.v
 read_xdc src/template.xdc 
-synth_design -top gvsp_ctrl_core -part xc7a100tfgg484-2 
+synth_design -top fb_write -part xc7a100tfgg484-2 
 report_utilization -file utilization_summary.rpt 
 report_timing_summary -file timing_summary.rpt 
