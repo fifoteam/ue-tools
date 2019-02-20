80850c2a178fedc1608573699fdd29c8743747c3
fifoteam <haitaox2008@163.com>
2019-02-20 21:22:24 +0800

    AUTO SAVED BY BAT FILE

---
+++ backup/Restore/vivado_ip_template.000
@@ -0,0 +1,16 @@
+
+echo %1
+cd /d %1
+
+::
+
+
+if exist managed_ip_project (
+	echo "folder exist"
+) else (
+	mkdir	managed_ip_project
+	copy "f:\kuaipan\kuaipan\FifoTeam\bat-tools\template_bat\vivado_ip_template\xc7a100tfgg484-2\managed_ip_project\managed_ip_project.xpr" "managed_ip_project"
+)
+
+
+::pause
--- ue/synplify/synthesis.tcl
+++ ue/synplify/synthesis.tcl
@@ -1,10 +1,10 @@
 project -new D:/tools/Synopsys/fpga_J-2015.03/bin/mbin/proj_1.prj
-add_file -verilog F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/frame_buffer/fb_read/fb_read_init.v
+add_file -verilog F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_platform/ver10/src/frame_buffer/fb_read/fb_read_core/reset_extend.v
 set_option -part XC7A100T
 set_option -grade -2
 set_option -package FGG484
 set_option -enable_nfilter 1
 project -log_file c:/ue/synplify/rev_2/proj_1.srr
-set_option -top_module fb_read_init
+set_option -top_module reset_extend
 project -save proj_1 c:/ue/synplify/proj_1.prj 
 project -run  -bg 
