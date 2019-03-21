296a7f08ff25e6884b22083141ece746fa43857a
fifoteam <haitaox2008@163.com>
2019-03-21 20:07:14 +0800

    AUTO SAVED BY BAT FILE

--- ue/synplify/synthesis.tcl
+++ ue/synplify/synthesis.tcl
@@ -1,10 +1,10 @@
 project -new D:/tools/Synopsys/fpga_J-2015.03/bin/mbin/proj_1.prj
-add_file -verilog F:/DAHENG/hw_mars/branches/xinghaitao/mars_platform/develop_stage/fpga_module/frame_info_add/src/frame_info_add.v
+add_file -verilog F:/DAHENG/hw_mars/branches/xinghaitao/5gige/mars-1231-49g5x-p/fpga_module/sensor_channel_sony/testbench/driver/common/test.v
 set_option -part XC7A100T
 set_option -grade -2
 set_option -package FGG484
 set_option -enable_nfilter 1
 project -log_file c:/ue/synplify/rev_2/proj_1.srr
-set_option -top_module frame_info_add
+set_option -top_module test
 project -save proj_1 c:/ue/synplify/proj_1.prj 
 project -run  -bg 
