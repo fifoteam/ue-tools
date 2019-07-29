8f5b1c1fb1afaa7d4c406e0e7c11a27291579452
fifoteam <haitaox2008@163.com>
2019-07-29 19:29:37 +0800

    AUTO SAVED BY BAT FILE

--- ue/synplify/synthesis.tcl
+++ ue/synplify/synthesis.tcl
@@ -1,10 +1,10 @@
 project -new D:/tools/Synopsys/fpga_J-2015.03/bin/mbin/proj_1.prj
-add_file -verilog F:/DAHENG/hw_mars/trunk/fpga_prj/mars-1230-23u3x/src/io_channel/trigger_delay.v
+add_file -verilog F:/DAHENG/hw_mars/branches/zhangxilun/gige/5-gige/fpga_module/axi4_lite_slave_ctrl/src/axi4_lite_slave_ctrl.v
 set_option -part XC7A100T
 set_option -grade -2
 set_option -package FGG484
 set_option -enable_nfilter 1
 project -log_file c:/ue/synplify/rev_2/proj_1.srr
-set_option -top_module trigger_delay
+set_option -top_module axi4_lite_slave_ctrl
 project -save proj_1 c:/ue/synplify/proj_1.prj 
 project -run  -bg 
