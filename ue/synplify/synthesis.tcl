59d18bd0f2864051cf6377c1cb65fb881897cb02
fifoteam <haitaox2008@163.com>
2019-08-20 19:11:45 +0800

    AUTO SAVED BY BAT FILE

--- ue/synplify/synthesis.tcl
+++ ue/synplify/synthesis.tcl
@@ -1,10 +1,10 @@
 project -new D:/tools/Synopsys/fpga_J-2015.03/bin/mbin/proj_1.prj
-add_file -verilog F:/DAHENG/hw_mars/mars_platform/TD/Development_Phase/003_develop_phase/fpga_module/fpga_module/gige_vision/gvcp_rx_filter/src/gvcp_rx_filter.v
+add_file -verilog F:/DAHENG/hw_mars/branches/liuyifu/fpga_module/i2c_master_top_updata/src/i2c_cmd_ctrl.v
 set_option -part XC7A100T
 set_option -grade -2
 set_option -package FGG484
 set_option -enable_nfilter 1
 project -log_file c:/ue/synplify/rev_2/proj_1.srr
-set_option -top_module gvcp_rx_filter
+set_option -top_module i2c_cmd_ctrl
 project -save proj_1 c:/ue/synplify/proj_1.prj 
 project -run  -bg 
